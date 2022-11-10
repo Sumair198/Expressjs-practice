4e0eee5c3dbb26b082731367358f6ed9820b2a8b
Sumair198 <sumairk198@gmail.com>
2022-11-10 12:32:50 +0500

    express-prac-2

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/',(req,res)=>
-{
-    res.send('hello from express sumair')
-})
-app.get('/contact',(req,res)=>
-{
-    res.send('Contact Page')
-})
-app.get('/about',(req,res)=>
-{
-    res.send('About Page')
-})
-app.listen(2000, ()=>
-{
-    console.log('listening on port 2000')
-})
+// app.get('/',(req,res)=>
+// {
+//     res.send('hello from express sumair')
+// })
+// app.get('/contact',(req,res)=>
+// {
+//     res.send('Contact Page')
+// })
+// app.get('/about',(req,res)=>
+// {
+//     res.send('About Page')
+// })
+// app.listen(2000, ()=>
+// {
+//     console.log('listening on port 2000')
+// })
+
+// const express = require('express')
+// const app = express()
+
+// const aboutPage = (req,res) => {
+//     res.send('Hello i am call back about page')
+// }
+// app.get('/', (req, res) => {
+//     res.send('hello from express sumair')
+// })
+// app.get('/contact', (req, res) => {
+//     res.send('Contact Page')
+// })
+// app.get('/about', aboutPage)
+
+
+// app.listen(2000, () => {
+//     console.log('listening on port 2000')
+// })
+
 
 //Static website using expressjs /get static
 
@@ -87,10 +107,11 @@ app.listen(2000, ()=>
 
 
 //404
+
 // const express = require('express')
 // const app = express()
 // const path = require('path')
-
+// const port = process.env.PORT || '4000'
 // app.set('view engine' ,'hbs')
 
 // const staticDynamic = path.join(__dirname,'staticweb')
@@ -98,7 +119,7 @@ app.listen(2000, ()=>
 
 // app.get('/',(req,res)=>
 // {
-//     res.render('index',) 
+//     res.render('index') 
 // })
 // app.get('/about' , (req , res)=>
 // {
@@ -110,7 +131,121 @@ app.listen(2000, ()=>
 //         errMsg : 'Oops Page Not Found'
 //     })
 // })
-// app.listen(2000, ()=>
+// app.listen(port, ()=>
 // {
-//     console.log('listening on port 2000')
+//     console.log(`Server listening at port ${port}`)
 // })
+
+//Practice-2
+//Routing
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || '5000'
+
+// app.get('/',(req,res)=>
+// {
+//     res.send('hello World!!!')
+// })
+// app.all('/all' , (req,res)=>
+// {
+//     res.send('All Method')
+// })
+// app.all('/api/*' , (req,res)=>
+// {
+//     res.send('Api page')
+// })
+// app.all('*' , (req,res)=>
+// {
+//     res.send('Page Not Found')
+// })
+
+//Path
+//string pattern path , only run in abcd and acd
+
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || '5000'
+
+// app.get('/ab?cd' , (req , res) =>
+// {
+//     res.send('hello practice')
+// })
+// app.listen(port , () =>
+// {
+//     console.log(`Server listening at port ${port}`)
+// })
+
+//RE
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || '5000'
+
+// app.get(/s/, (req , res) =>
+// {
+//     res.send('( s ) found')
+// })
+
+//callbacks
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || '5000'
+
+//more than one call back
+
+// app.get('/cbexample', (req, res, next) => {
+//     console.log('first callback')
+//     next()
+// }
+//     , (req, res) => {
+//         console.log('Second callback')
+//         res.send('more than one callback')
+//     })
+
+//Array of callback
+// const cb1 = (req,res,next)=>
+// {
+//     console.log('first callback')
+//     next()
+// }
+// const cb2 = (req,res,next)=>
+// {
+//     console.log('second callback')
+//     next()
+// }
+// const cb3 = (req,res)=>
+// {
+//     console.log('third callback')
+//     res.send('Array Of Callbacks')
+// }
+// app.get('/array' , [cb1,cb2,cb3])
+
+//chained route //Path same method change only
+const express = require('express')
+const app = express()
+const port = process.env.PORT || '5000'
+
+app.route('/student')
+    .all((req, res,next) => {
+        console.log('first')
+        res.send('All method')
+        next()
+    })
+    .get((req, res) => {
+        console.log('get method')
+        res.send('All student')
+    })
+
+    .post((req, res) => {
+        console.log('Post method')
+        res.send('Add Student')
+    })
+    .put((req, res) => {
+        console.log('Put method')
+        res.send('Update student')
+    })
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening at port ${port}`)
+})
